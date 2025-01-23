71d3347f9f9fa9eda39aed40db3b05821e24b941
Truls Johan Jacobsen-Lamøy <truls@Trulss-MBP.home>
2025-01-23 19:43:00 +0100

    chore: added index.html in cors origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 const SECRET = process.env.SECRET || 'topsecret';
 const app = express();
 
-app.use(cors({ origin: 'http://127.0.0.1:5501' }));
+app.use(cors({ origin: 'http://127.0.0.1:5501/index.html' }));
 app.use(express.json());
 
 function authenticateToken(req, res, next) {
